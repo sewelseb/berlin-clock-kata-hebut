809ec010a035a616a008ca830aa9b6deb2666760
sewelseb <sewelseb@hotmail.com>
2021-04-21 08:20:18 +0200

    init

---
+++ test.js
@@ -0,0 +1,13 @@
+const chai = require('chai');
+
+const should = chai.should();
+
+describe("Berlin clock kata project", function () {
+    context("Single minute row method", function() {
+        it("demo test", function() {
+            var actural = true;
+
+            actural.should.equal(true);
+        })
+    })
+});
