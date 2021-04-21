d73c2f7a6a2fcac7d7219c61ae09424eab72ff4f
sewelseb <sewelseb@hotmail.com>
2021-04-21 08:29:46 +0200

    add - manage 0min cas

---
+++ convertor.js
@@ -0,0 +1,5 @@
+module.exports = {
+    getSingleMinuteRow(time) {
+        return "0000";
+    }
+}
--- test.js
+++ test.js
@@ -1,13 +1,19 @@
 const chai = require('chai');
+const Convertor  = require('./convertor');
 
 const should = chai.should();
 
+// "YYYY"
+// "0000"
+// "Y000"
+
 describe("Berlin clock kata project", function () {
     context("Single minute row method", function() {
-        it("demo test", function() {
-            var actural = true;
+        it("given 00:00:00 should return 0000", function() {
 
-            actural.should.equal(true);
-        })
-    })
+            var actural = Convertor.getSingleMinuteRow("00:00:00");
+
+            actural.should.equal("0000");
+        });
+    });
 });
