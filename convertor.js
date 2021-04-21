c0b28ecb528c323438ff4cc035e0d7fd07e6b75e
sewelseb <sewelseb@hotmail.com>
2021-04-21 08:56:39 +0200

    refactor - extract get yellow lamps

--- convertor.js
+++ convertor.js
@@ -1,13 +1,21 @@
 module.exports = {
     getSingleMinuteRow(time) {
         const minutes = this.getMinutes(time);
-        if(minutes === 1) return "Y000";
-        if(minutes === 2) return "YY00";
-        if(minutes === 3) return "YYY0";
+        if(minutes === 1) return this.getYellowLampsForMinutes(minutes) + "000";
+        if(minutes === 2) return this.getYellowLampsForMinutes(minutes) + "00";
+        if(minutes === 3) return this.getYellowLampsForMinutes(minutes) + "0";
 
         return "0000";
     },
     getMinutes(time) {
         return parseInt(time.split(':')[1]);
+    },
+    getYellowLampsForMinutes(minutes) {
+        var yellowLamps = "";
+        for (let i = 0; i < minutes; i++) {
+            yellowLamps += "Y";          
+        }
+
+        return yellowLamps;
     }
 }
