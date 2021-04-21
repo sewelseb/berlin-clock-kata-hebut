4d032750e16164623d2d836f8e3fd87c0e842958
sewelseb <sewelseb@hotmail.com>
2021-04-21 08:32:53 +0200

    add - manage case 2 minutes

--- convertor.js
+++ convertor.js
@@ -1,5 +1,7 @@
 module.exports = {
     getSingleMinuteRow(time) {
+        if(time === "00:01:00") return "Y000";
+
         return "0000";
     }
 }
--- test.js
+++ test.js
@@ -10,10 +10,14 @@ const should = chai.should();
 describe("Berlin clock kata project", function () {
     context("Single minute row method", function() {
         it("given 00:00:00 should return 0000", function() {
+            var actual = Convertor.getSingleMinuteRow("00:00:00");
 
-            var actural = Convertor.getSingleMinuteRow("00:00:00");
+            actual.should.equal("0000");
+        });
+        it("given 00:01:00 should return Y000", function() {
+            actual = Convertor.getSingleMinuteRow("00:01:00");
 
-            actural.should.equal("0000");
+            actual.should.equal("Y000");
         });
     });
 });
